43a51c5f8950b66be2d35a1090bea7a55e699bb0
GoldenRare <deshawnmohan@hotmail.com>
2024-01-06 21:19:56 -0500

    Support for Zobrist Hashing

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include "move_generator.h"
 #include "chess_board.h"
 #include "uci.h"
+#include "zobrist.h"
 
 int main () {
     initializeMoveGenerator();
     initializeChessBoard();
+    initializeZobrist();
     uciLoop();
     return 0;
 }
