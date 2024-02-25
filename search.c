6918e7011d3fc97b5858cf58b0797e1115322e12
GoldenRare <deshawnmohan@hotmail.com>
2024-02-25 15:54:10 -0500

    Initial support for Transposition Table in Search

--- search.c
+++ search.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <time.h>
 #include "search.h"
 #include "chess_board.h"
 #include "utility.h"
 #include "move_generator.h"
 #include "evaluation.h"
+#include "transposition_table.h"
 
 void startSearch(ChessBoard *board, int depth) {
     int bestScore = -INFINITE;
@@ -39,32 +41,61 @@ void startSearch(ChessBoard *board, int depth) {
 }
 
 int alphaBeta(ChessBoard *board, int alpha, int beta, int depth) {
+    /* Transposition Table */
+    bool hasEvaluation;
+    PositionEvaluation *pe = probeTranspositionTable(board->positionKey, &hasEvaluation);
+    if (hasEvaluation) {
+        Bound bound = getBound(pe);
+        int nodeScore = pe->nodeScore;
+        if (pe->depth >= depth) {
+            // TODO: Consider optimizing the below and need to adjust node score
+            if ((bound == LOWER && nodeScore >= beta) || (bound == UPPER && nodeScore <= alpha) || bound == EXACT) {
+                return nodeScore; // Fail Soft
+            }
+        } 
+    }
+    /*                     */
 
-   if (depth == 0) return evaluation(board);
-   
+    /* Quiescence Search */
+    if (depth == 0) return evaluation(board); //TODO: Test this before TT probe
+    /*                   */
+
+    /* Main Moves Loop */
     IrreversibleBoardState ibs;
     Move moveList[256];
     Move *endList = createMoveList(board, moveList);
     Bitboard pinnedPieces = getPinnedPieces(board);
-    int score = -INFINITE;
-    int legalMoves = 0;
+
+    int bestScore = -INFINITE;
+    Move bestMove = NO_MOVE;
     for (Move *move = moveList; move != endList; move++) {
         if (!isLegalMove(board, move, pinnedPieces)) continue;
-        legalMoves++;
         makeMove(board, move, &ibs);
-        score = -alphaBeta(board, -beta, -alpha, depth - 1);
+        int score = -alphaBeta(board, -beta, -alpha, depth - 1);
         undoMove(board, move, &ibs);
 
-        if (score >= beta) return beta; // Fail hard
-        if (score > alpha) alpha = score;
+        // bestScore <= alpha < beta
+        if (score > bestScore) {
+            bestScore = score;
+            if (score > alpha) {
+                if (score >= beta) {
+                    savePositionEvaluation(pe, board->positionKey, *move, depth, LOWER, score);
+                    return score; // Fail Soft
+                }
+                bestMove = *move;
+                alpha = score; 
+            }
+        }
     }
+    /*                 */
 
-    /* CHECKMATE AND STALEMATE DETECTION */
-    if (!legalMoves) {
+    /* Checkmate and Stalemate Detection */
+    if (bestScore == -INFINITE) { // Something is always better than nothing, so bestScore stays -INFINITE if it couldn't play any move
         Colour stm = board->sideToMove;
-        alpha = attackersTo(board, getKingSquare(board, stm), stm, getOccupiedSquares(board)) ? CHECKMATED - depth : DRAW; // Temporary solution
+        bestScore = attackersTo(board, getKingSquare(board, stm), stm, getOccupiedSquares(board)) ? CHECKMATED - depth : DRAW; // TODO: Temporary solution, should this be considered EXACT bound?
     }
     /*                                   */
 
-    return alpha;
+    savePositionEvaluation(pe, board->positionKey, bestMove, depth, bestMove != NO_MOVE ? EXACT : UPPER, bestScore);
+    return bestScore; // Fail Soft
 }
