579d1f2a4fee3610075eab5796bdfed7a3160346
GoldenRare <deshawnmohan@hotmail.com>
2024-03-16 17:42:56 -0400

    Draft for higher level move selection

---
+++ move_selector.h
@@ -0,0 +1,27 @@
+#ifndef MOVE_SELECTOR_H
+#define MOVE_SELECTOR_H
+
+#include "chess_board.h"
+#include "utility.h"
+
+enum MoveSelectorState {
+    TT_MOVE, ALL_MOVES_INIT, ALL_MOVES
+};
+typedef enum MoveSelectorState MoveSelectorState;
+
+typedef struct MoveSelector {
+    Move *startList;
+    Move *endList;
+    MoveSelectorState state;
+    Move ttMove;
+    Move moveList[256];
+} MoveSelector;
+
+inline void createMoveSelector(MoveSelector *ms, MoveSelectorState state, Move ttMove) {
+    ms->state = state;
+    ms->ttMove = ttMove;
+}
+
+Move getNextBestMove(const ChessBoard *board, MoveSelector *ms);
+
+#endif
