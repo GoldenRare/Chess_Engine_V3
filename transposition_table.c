c506ce74b54f8693a057c8187a6eecf8a58a7bbc
GoldenRare <deshawnmohan@hotmail.com>
2025-06-12 16:15:00 -0400

    Initialize default size of TT

--- transposition_table.c
+++ transposition_table.c
@@ -1,31 +1,34 @@
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <stdbool.h>
 #include "transposition_table.h"
 #include "utility.h"
 
 TranspositionTable TT;
 
+void initializeTranspositionTable() {
+    setTranspositionTableSize(256);
+}
+
 void setTranspositionTableSize(size_t MB) {
-    free(TT.table);
-    size_t numberOfBuckets = MB * 1024 * 1024 / sizeof(Bucket); // Convert MB to B then, divide by the size of 1 bucket
+    free(TT.buckets);
+    size_t numberOfBuckets = MB * 1024 * 1024 / sizeof(PEBucket); // Convert MB to B then, divide by the size of 1 bucket
     
     // Rounds down to the nearest largest power of 2, this may cause substantially less space allocation than what was requested
     numberOfBuckets = squareToBitboard(bitboardToSquareMSB(numberOfBuckets)); 
 
-    TT.table = (Bucket*) calloc(numberOfBuckets, sizeof(Bucket));
-    if (TT.table == NULL) {
+    TT.buckets = calloc(numberOfBuckets, sizeof(PEBucket));
+    if (!TT.buckets) {
         printf("info string Memory Allocation Failed! Exiting ...\n"); 
         exit(1);
     }
     
-    printf("info string Allocating %llu MB of memory.\n", numberOfBuckets * sizeof(Bucket) / 1024 / 1024);
+    printf("info string Allocating %llu MB of memory.\n", numberOfBuckets * sizeof(PEBucket) / 1024 / 1024);
     TT.mask = numberOfBuckets - 1;
 }
 
-PositionEvaluation* probeTranspositionTable(Key positionKey, bool *hasEvaluation) {
-    PositionEvaluation* pe = &TT.table[positionKey & TT.mask].pe[0];
+PositionEvaluation* probeTranspositionTable(Key positionKey, bool *restrict hasEvaluation) {
+    PositionEvaluation* pe = &TT.buckets[positionKey & TT.mask].pe[0];
     uint16_t keyIndex = positionKey >> 48;
 
     for (size_t i = 0; i < BUCKET_SIZE; i++) {
--- transposition_table.h
+++ transposition_table.h
@@ -1,46 +1,45 @@
 #ifndef TRANSPOSITION_TABLE_H
 #define TRANSPOSITION_TABLE_H
 
-#define BUCKET_SIZE 3
-
 #include <stddef.h>
 #include <stdint.h>
-#include <stdbool.h>
 #include "utility.h"
+
+constexpr int BUCKET_SIZE = 3; // TODO: Find optimal size
 
 typedef struct PositionEvaluation {
     uint16_t key;
-    uint16_t bestMove;
-    uint8_t depth;
-    uint8_t ageBounds;
     int16_t nodeScore;
+    Move bestMove;
+    uint8_t depth; // TODO: Consider QSearch depth, it is not unsigned since QSearch depth is negative
+    uint8_t ageBounds; // TODO: Does not consider aging
 } PositionEvaluation;
 
-typedef struct Bucket {
-    PositionEvaluation pe[BUCKET_SIZE];
-} Bucket;
+typedef struct PEBucket {
+    PositionEvaluation pe[BUCKET_SIZE]; // TODO: Find optimal size
+} PEBucket;
 
 typedef struct TranspositionTable {
+    PEBucket *buckets;
     uint64_t mask; // TODO: Could mask be smaller?
-    Bucket *table;
 } TranspositionTable;
-
 extern TranspositionTable TT;
 
-inline Bound getBound(const PositionEvaluation *pe) {
+static inline Bound getBound(const PositionEvaluation *pe) {
     return pe->ageBounds & 0x3;
 }
 
 // TODO: Need to ensure that function is called correctly due to type conversions
-inline void savePositionEvaluation(PositionEvaluation *pe, Key positionKey, Move bestMove, uint8_t depth, Bound bound, int16_t nodeScore) {
+static inline void savePositionEvaluation(PositionEvaluation *pe, Key positionKey, Move bestMove, uint8_t depth, Bound bound, int16_t nodeScore) {
     pe->key = positionKey >> 48;
     pe->bestMove = bestMove;
     pe->depth = depth; // TODO: Consider QSearch depth, it is not unsigned since QSearch depth is negative
-    pe->ageBounds = bound; // TODO: Does not consider aging
+    pe->ageBounds = bound;
     pe->nodeScore = nodeScore;
 }
 
+void initializeTranspositionTable();
 void setTranspositionTableSize(size_t MB);
-PositionEvaluation* probeTranspositionTable(Key positionKey, bool *hasEvaluation);
+PositionEvaluation* probeTranspositionTable(Key positionKey, bool *restrict hasEvaluation);
 
 #endif
