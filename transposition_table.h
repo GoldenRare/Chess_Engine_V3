88e06e20d11dc944a49708915d53180fdccd135f
GoldenRare <deshawnmohan@hotmail.com>
2025-06-25 04:43:57 -0400

    Introduces aging into the transposition table and protects more valuable entries

--- transposition_table.h
+++ transposition_table.h
@@ -11,8 +11,8 @@ typedef struct PositionEvaluation {
     uint16_t key;
     int16_t nodeScore;
     Move bestMove;
-    uint8_t depth; // TODO: Consider QSearch depth, it is not unsigned since QSearch depth is negative
-    uint8_t ageBounds; // TODO: Does not consider aging
+    Depth depth;
+    uint8_t ageBounds;
 } PositionEvaluation;
 
 typedef struct PEBucket {
@@ -22,20 +22,43 @@ typedef struct PEBucket {
 typedef struct TranspositionTable {
     PEBucket *buckets;
     uint64_t mask; // TODO: Could mask be smaller?
+    uint8_t age;
 } TranspositionTable;
 extern TranspositionTable TT;
+
+static inline void startNewSearch() {
+    TT.age++;
+}
 
 static inline Bound getBound(const PositionEvaluation *pe) {
     return pe->ageBounds & 0x3;
 }
 
+static inline Score adjustNodeScoreToTT(Score nodeScore, int ply) {
+    return nodeScore >=  GUARANTEE_CHECKMATE ? nodeScore + ply
+         : nodeScore <= -GUARANTEE_CHECKMATE ? nodeScore - ply
+         : nodeScore;
+}
+
+static inline Score adjustNodeScoreFromTT(Score nodeScore, int ply) {
+    return nodeScore >=  GUARANTEE_CHECKMATE ? nodeScore - ply
+         : nodeScore <= -GUARANTEE_CHECKMATE ? nodeScore + ply
+         : nodeScore;
+}
+
 // TODO: Need to ensure that function is called correctly due to type conversions
-static inline void savePositionEvaluation(PositionEvaluation *pe, Key positionKey, Move bestMove, uint8_t depth, Bound bound, int16_t nodeScore) {
-    pe->key = positionKey >> 48;
-    pe->bestMove = bestMove;
-    pe->depth = depth; // TODO: Consider QSearch depth, it is not unsigned since QSearch depth is negative
-    pe->ageBounds = bound;
-    pe->nodeScore = nodeScore;
+static inline void savePositionEvaluation(PositionEvaluation *pe, Key positionKey, Move bestMove, Depth depth, Bound bound, int16_t nodeScore) {
+    uint16_t positionKeyIndex = positionKey >> 48;
+    // TODO: How much to value an exact bound? Or even potentially other bounds?
+    // Protect more valuable data from being overwritten
+    if (positionKeyIndex != pe->key || depth > pe->depth) {
+        pe->bestMove = bestMove;
+        pe->depth = depth;
+        pe->ageBounds = bound;
+        pe->nodeScore = nodeScore;
+    }
+    pe->ageBounds = TT.age << 2 | getBound(pe);
+    pe->key = positionKeyIndex;
 }
 
 void initializeTranspositionTable();
