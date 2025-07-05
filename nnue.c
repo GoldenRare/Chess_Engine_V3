9527fb11f4b5bee026f6a1f285fec13c7d1a0cd7
GoldenRare <deshawnmohan@hotmail.com>
2025-07-05 17:58:13 -0400

    Remove global accumulator for local ones

--- nnue.c
+++ nnue.c
@@ -1,29 +1,9 @@
 #include <stdint.h>
 #include <stdio.h>
+#include "nnue.h"
 #include "utility.h"
 
-constexpr int FLIP_MASK = 0b111000;
-
-constexpr int SCORE_SCALE     = 400;
-constexpr int QUANTIZATION_A  = 255;
-constexpr int QUANTIZATION_B  =  64;
-
-constexpr int PERSPECTIVE = 2;
-constexpr int LAYER1 = 128;
-
-typedef struct Network {
-    int16_t accumulatorWeights[COLOURS][PIECE_TYPES - 1][SQUARES][LAYER1];
-    int16_t accumulatorBiases[LAYER1];
-
-    int16_t outputWeights[LAYER1 * PERSPECTIVE];
-    int16_t outputBias;
-} Network;
 static Network network; // TODO: Can we make it const?
-
-typedef struct Accumulator {
-    int16_t accumulator[COLOURS][LAYER1];
-} Accumulator;
-static Accumulator accumulator;
 
 static inline int32_t SCReLU(int16_t val) {
     return val >= QUANTIZATION_A ? QUANTIZATION_A * QUANTIZATION_A 
@@ -32,59 +12,59 @@ static inline int32_t SCReLU(int16_t val) {
 }
 
 void initializeNNUE() {
-    FILE *file = fopen("temp.bin", "rb");
+    FILE *file = fopen("nnue.bin", "rb");
     fread(&network, sizeof(Network), 1 , file);
     fclose(file);
 }
 
-void resetAccumulator() {
+void accumulatorReset(Accumulator *restrict accumulator) {
     for (int i = 0; i < LAYER1; i++) {
-        accumulator.accumulator[WHITE][i] = network.accumulatorBiases[i];
-        accumulator.accumulator[BLACK][i] = network.accumulatorBiases[i];
+        accumulator->accumulator[WHITE][i] = network.accumulatorBiases[i];
+        accumulator->accumulator[BLACK][i] = network.accumulatorBiases[i];
     }
 }
 
-void accumulatorAdd(Colour c, PieceType pt, Square sq) {
+void accumulatorAdd(Accumulator *restrict accumulator, Colour c, PieceType pt, Square sq) {
     pt--;
     for (int i = 0; i < LAYER1; i++) {
-        accumulator.accumulator[WHITE][i] += network.accumulatorWeights[c][pt][sq ^ FLIP_MASK][i];
+        accumulator->accumulator[WHITE][i] += network.accumulatorWeights[c][pt][sq ^ FLIP_MASK][i];
     }
 
     for (int i = 0; i < LAYER1; i++) {
-        accumulator.accumulator[BLACK][i] += network.accumulatorWeights[c ^ 1][pt][sq][i];
+        accumulator->accumulator[BLACK][i] += network.accumulatorWeights[c ^ 1][pt][sq][i];
     }
 }
 
-void accumulatorSub(Colour c, PieceType pt, Square sq) {
+void accumulatorSub(Accumulator *restrict accumulator, Colour c, PieceType pt, Square sq) {
     pt--;
     for (int i = 0; i < LAYER1; i++) {
-        accumulator.accumulator[WHITE][i] -= network.accumulatorWeights[c][pt][sq ^ FLIP_MASK][i];
+        accumulator->accumulator[WHITE][i] -= network.accumulatorWeights[c][pt][sq ^ FLIP_MASK][i];
     }
 
     for (int i = 0; i < LAYER1; i++) {
-        accumulator.accumulator[BLACK][i] -= network.accumulatorWeights[c ^ 1][pt][sq][i];
+        accumulator->accumulator[BLACK][i] -= network.accumulatorWeights[c ^ 1][pt][sq][i];
     }
 }
 
-void accumulatorAddSub(Colour c, PieceType pt, Square fromSquare, Square toSquare) {
+void accumulatorAddSub(Accumulator *restrict accumulator, Colour c, PieceType pt, Square fromSquare, Square toSquare) {
     pt--;
     for (int i = 0; i < LAYER1; i++) {
-        accumulator.accumulator[WHITE][i] += network.accumulatorWeights[c][pt][toSquare ^ FLIP_MASK][i] - network.accumulatorWeights[c][pt][fromSquare ^ FLIP_MASK][i];
+        accumulator->accumulator[WHITE][i] += network.accumulatorWeights[c][pt][toSquare ^ FLIP_MASK][i] - network.accumulatorWeights[c][pt][fromSquare ^ FLIP_MASK][i];
     }
 
     for (int i = 0; i < LAYER1; i++) {
-        accumulator.accumulator[BLACK][i] += network.accumulatorWeights[c ^ 1][pt][toSquare][i] - network.accumulatorWeights[c ^ 1][pt][fromSquare][i];
+        accumulator->accumulator[BLACK][i] += network.accumulatorWeights[c ^ 1][pt][toSquare][i] - network.accumulatorWeights[c ^ 1][pt][fromSquare][i];
     }
 }
 
-Score evaluation(Colour stm) {
+Score evaluation(const Accumulator *restrict accumulator, Colour stm) {
     Score score = 0;
     for (int i = 0; i < LAYER1; i++) {
-        score += SCReLU(accumulator.accumulator[stm][i]) * network.outputWeights[i];
+        score += SCReLU(accumulator->accumulator[stm][i]) * network.outputWeights[i];
     }
 
     for (int i = 0; i < LAYER1; i++) {
-        score += SCReLU(accumulator.accumulator[stm ^ 1][i]) * network.outputWeights[i + LAYER1];
+        score += SCReLU(accumulator->accumulator[stm ^ 1][i]) * network.outputWeights[i + LAYER1];
     }
 
     score /= QUANTIZATION_A;
--- nnue.h
+++ nnue.h
@@ -3,17 +3,38 @@
 
 #include "utility.h"
 
+constexpr int FLIP_MASK = 0b111000;
+
+constexpr int SCORE_SCALE     = 400;
+constexpr int QUANTIZATION_A  = 255;
+constexpr int QUANTIZATION_B  =  64;
+
+constexpr int PERSPECTIVE = 2;
+constexpr int LAYER1 = 128;
+
+typedef struct Network {
+    int16_t accumulatorWeights[COLOURS][PIECE_TYPES - 1][SQUARES][LAYER1];
+    int16_t accumulatorBiases[LAYER1];
+
+    int16_t outputWeights[LAYER1 * PERSPECTIVE];
+    int16_t outputBias;
+} Network;
+
+typedef struct Accumulator {
+    int16_t accumulator[COLOURS][LAYER1];
+} Accumulator;
+
 void initializeNNUE();
 
-void resetAccumulator();
+void accumulatorReset(Accumulator *restrict accumulator);
 // Adds a piece to the accumulator, always using the perspective of white.
-void accumulatorAdd(Colour c, PieceType pt, Square sq);
+void accumulatorAdd(Accumulator *restrict accumulator, Colour c, PieceType pt, Square sq);
 // Removes a piece from the accumulator, always using the perspective of white.
-void accumulatorSub(Colour c, PieceType pt, Square sq);
+void accumulatorSub(Accumulator *restrict accumulator, Colour c, PieceType pt, Square sq);
 // Adds a piece to the accumulator, and removes it from the square it was previously on.
 // Always using the perspective of white.
-void accumulatorAddSub(Colour c, PieceType pt, Square fromSquare, Square toSquare);
+void accumulatorAddSub(Accumulator *restrict accumulator, Colour c, PieceType pt, Square fromSquare, Square toSquare);
 
-Score evaluation(Colour stm);
+Score evaluation(const Accumulator *restrict accumulator, Colour stm);
 
 #endif
