46687984dce9d064b7ddfaf28b6a68c3278740f7
GoldenRare <deshawnmohan@hotmail.com>
2025-07-11 15:28:13 -0400

    Constructor to create a SearchThread

--- search.h
+++ search.h
@@ -1,15 +1,20 @@
 #ifndef SEARCH_H
 #define SEARCH_H
 
+#include <stddef.h>
 #include "chess_board.h"
 #include "transposition_table.h"
 #include "utility.h"
 
+constexpr Depth MAX_DEPTH = 8;
+
 typedef struct SearchThread {
-    TT *tt;
+    TT tt;
 } SearchThread;
 
-constexpr Depth MAX_DEPTH = 8;
+static inline void createSearchThread(SearchThread *restrict st, size_t MB) {
+    createTranspositionTable(&st->tt, MB);
+}
 
 MoveObject startSearch(ChessBoard *restrict board, Depth depth, SearchThread *st);
 
