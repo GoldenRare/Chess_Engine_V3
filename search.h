89f1c5c13c64ea8dfb01cf5504445a5e7d177611
GoldenRare <deshawnmohan@hotmail.com>
2025-07-17 13:40:22 -0400

    Update maximum depth to 5

--- search.h
+++ search.h
@@ -6,14 +6,22 @@
 #include "transposition_table.h"
 #include "utility.h"
 
-constexpr Depth MAX_DEPTH = 8;
+constexpr Depth MAX_DEPTH = 5;
 
 typedef struct SearchThread {
     TT tt;
+    bool print;
 } SearchThread;
 
-static inline void createSearchThread(SearchThread *restrict st, size_t MB) {
-    createTranspositionTable(&st->tt, MB);
+// When creating a new search thread, ensure that a call to destroySearchThread() is made when the thread is no longer needed
+static inline void createSearchThread(SearchThread *restrict st, size_t hashSize, bool print) {
+    createTranspositionTable(&st->tt, hashSize);
+    st->print = print;
+}
+
+// Destroys a previously created search thread, ensure that a call to createSearchThread() is made before using this function
+static inline void destroySearchThread(SearchThread *st) {
+    destroyTranspositionTable(&st->tt);
 }
 
 MoveObject startSearch(ChessBoard *restrict board, Depth depth, SearchThread *st);
