003453e69afd22752bba4a6ab256821a960caa36
GoldenRare <deshawnmohan@hotmail.com>
2025-10-17 18:59:27 -0400

    Move functions to header file

--- transposition_table.c
+++ transposition_table.c
@@ -1,37 +1,12 @@
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
+#include <stdint.h>
 #include "transposition_table.h"
 #include "utility.h"
 
-TranspositionTable TT;
-
-void initializeTranspositionTable() {
-    setTranspositionTableSize(256);
-}
-
-void setTranspositionTableSize(size_t MB) {
-    free(TT.buckets);
-    size_t numberOfBuckets = MB * 1024 * 1024 / sizeof(PEBucket); // Convert MB to B then, divide by the size of 1 bucket
-    
-    // Rounds down to the nearest largest power of 2, this may cause substantially less space allocation than what was requested
-    numberOfBuckets = squareToBitboard(bitboardToSquareMSB(numberOfBuckets)); 
-
-    TT.buckets = calloc(numberOfBuckets, sizeof(PEBucket));
-    if (!TT.buckets) {
-        printf("info string Memory Allocation Failed! Exiting ...\n"); 
-        exit(1);
-    }
-    
-    printf("info string Allocating %llu MB of memory.\n", numberOfBuckets * sizeof(PEBucket) / 1024 / 1024);
-    TT.mask = numberOfBuckets - 1;
-}
-
-PositionEvaluation* probeTranspositionTable(Key positionKey, bool *restrict hasEvaluation) {
-    PositionEvaluation* pe = &TT.buckets[positionKey & TT.mask].pe[0];
+PositionEvaluation* probeTranspositionTable(const TT *tt, Key positionKey, bool *restrict hasEvaluation) {
+    PositionEvaluation* pe = &tt->buckets[positionKey & tt->mask].pe[0];
     uint16_t keyIndex = positionKey >> 48;
 
-    for (size_t i = 0; i < BUCKET_SIZE; i++) {
+    for (int i = 0; i < BUCKET_SIZE; i++) {
         if (pe[i].key == keyIndex || !pe[i].key) {
             *hasEvaluation = pe[i].key;
             return &pe[i];
--- transposition_table.h
+++ transposition_table.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 #include <stdint.h>
+#include <string.h>
 #include "utility.h"
 
 constexpr int BUCKET_SIZE = 3; // TODO: Find optimal size
@@ -15,19 +16,31 @@ typedef struct PositionEvaluation {
     uint8_t ageBounds;
 } PositionEvaluation;
 
-typedef struct PEBucket {
-    PositionEvaluation pe[BUCKET_SIZE]; // TODO: Find optimal size
+typedef struct PositionEvaluationBucket {
+    PositionEvaluation pe[BUCKET_SIZE];
 } PEBucket;
 
 typedef struct TranspositionTable {
     PEBucket *buckets;
     uint64_t mask; // TODO: Could mask be smaller?
     uint8_t age;
-} TranspositionTable;
-extern TranspositionTable TT;
+} TT;
 
-static inline void startNewSearch() {
-    TT.age++;
+// Can be called multiple times but the first call must have tt->buckets == nullptr for the forced call to free()
+static inline void createTranspositionTable(TT *restrict tt, size_t MB) {
+    size_t numberOfBuckets = MB * 1024 * 1024 / sizeof(PEBucket); // Convert MB to B
+    
+    // Rounds down to the nearest largest power of 2, this may cause substantially less space allocation than what was requested
+    numberOfBuckets = squareToBitboard(bitboardToSquareMSB(numberOfBuckets));
+    free(tt->buckets);
+    tt->buckets = calloc(numberOfBuckets, sizeof(PEBucket));
+    tt->mask = numberOfBuckets - 1;
+    tt->age = -1;
+}
+
+static inline void clearTranspositionTable(TT *restrict tt) {
+    memset(tt->buckets, 0, sizeof(PEBucket) * (tt->mask + 1));
+    tt->age = -1;
 }
 
 static inline Bound getBound(const PositionEvaluation *pe) {
@@ -47,7 +60,7 @@ static inline Score adjustNodeScoreFromTT(Score nodeScore, int ply) {
 }
 
 // TODO: Need to ensure that function is called correctly due to type conversions
-static inline void savePositionEvaluation(PositionEvaluation *pe, Key positionKey, Move bestMove, Depth depth, Bound bound, int16_t nodeScore) {
+static inline void savePositionEvaluation(TT *tt, PositionEvaluation *pe, Key positionKey, Move bestMove, Depth depth, Bound bound, int16_t nodeScore) {
     uint16_t positionKeyIndex = positionKey >> 48;
     // TODO: How much to value an exact bound? Or even potentially other bounds?
     // Protect more valuable data from being overwritten
@@ -57,12 +70,10 @@ static inline void savePositionEvaluation(PositionEvaluation *pe, Key positionKe
         pe->ageBounds = bound;
         pe->nodeScore = nodeScore;
     }
-    pe->ageBounds = TT.age << 2 | getBound(pe);
+    pe->ageBounds = tt->age << 2 | getBound(pe);
     pe->key = positionKeyIndex;
 }
 
-void initializeTranspositionTable();
-void setTranspositionTableSize(size_t MB);
-PositionEvaluation* probeTranspositionTable(Key positionKey, bool *restrict hasEvaluation);
+PositionEvaluation* probeTranspositionTable(const TT *tt, Key positionKey, bool *restrict hasEvaluation);
 
 #endif
